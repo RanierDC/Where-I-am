43a4a0e14877ad9baff2bd6f18cbca4ad5bc456a
Ranier Dalton do Couto <ranierd.couto@gmail.com>
2022-11-12 21:24:34 -0300

    1.9
    - Sistema de coleta de chaves quase completa

---
+++ Labirinto-Terror/Assets/Scripts/keyScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyScript : MonoBehaviour
+{
+    public GameObject inticon, key;
+	
+	void OnTriggerStay(Collider other){
+		if(other.CompareTag("MainCamera")){
+			inticon.SetActive(true);
+			if(Input.GetKeyDown(KeyCode.E)){
+				key.SetActive(false);
+				DoorScript.keyfound = true;
+				inticon.SetActive(false);
+			}
+		}
+	}
+    
+	void OnTriggerExit(Collider other){
+		if(other.CompareTag("MainCamera")){
+			inticon.SetActive(false);
+		}
+	}
+}
